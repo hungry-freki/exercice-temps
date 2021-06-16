cf0cb7748a50d6272ad18c6d558add7536792228
trashpanda <juliedepach@gmail.com>
2021-06-16 11:48:37 +0200

    essai arguments

--- temps.py
+++ temps.py
@@ -1,9 +1,7 @@
 import sys
 
 def time(h, m, s):
-    h = 60
-    m = 60
-    s = 60
+
     while s > 59 or m > 59:
         if s > 59:
             sec = s // 60
@@ -14,6 +12,6 @@ def time(h, m, s):
             m = m % 60
             h = min + h
     return (h, m, s)
-#__name__=="__main__"
+__name__=="__main__"
 
-print(time)
+print(time[1, 60, 60])
