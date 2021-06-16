857f566ea75a88264d2db034cf20dc48230d9212
trashpanda <juliedepach@gmail.com>
2021-06-16 11:40:24 +0200

    changememt nom variables

--- temps.py
+++ temps.py
@@ -6,13 +6,13 @@ def time(h, m, s):
     s = 60
     while s > 59 or m > 59:
         if s > 59:
-            S = s // 60
+            sec = s // 60
             s = s % 60
-            m = S + m
+            m = sec + m
         if m > 59:
-            M = m // 60
+            min = m // 60
             m = m % 60
-            h = M + h
+            h = min + h
     return (h, m, s)
 #__name__=="__main__"
 
